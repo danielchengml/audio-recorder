17a5b6fda47a91809a54069761db32b62c3dc37a
Daniel Cheng <danielchengml@gmail.com>
2018-05-08 11:45:40 -0700

    refactor before deployment

--- src/components/Recordings.js
+++ src/components/Recordings.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
 import Card, { CardActions, CardContent } from "material-ui/Card";
 import Button from "material-ui/Button";
 import Typography from "material-ui/Typography";
